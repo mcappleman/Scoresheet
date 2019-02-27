bc1149d192d0911a62887e0789ea53e0ccd4ae29
Matthew Cappleman <matt.cappleman@gmail.com>
2019-02-27 16:15:36 -0500

    get batting from players

--- Scoresheet/Scoresheet.py
+++ Scoresheet/Scoresheet.py
@@ -16,22 +16,30 @@ df = pd.read_csv(CSV_PATH)
 column_order = list(df)
 # driver = webdriver.Chrome()
 
+players = []
+
 for index, row in df.iterrows():
     id = row['ID']
     base_url = "https://www.baseball-reference.com/players"
     ending = "/" + id[0] + "/" + id + ".shtml"
     player_page = requests.get(base_url + ending)
+
     bats_text = ""
     regex = r"Bats:.*strong>(?P<BATS>.*)\s"
     matches = re.finditer(regex, player_page.text, re.MULTILINE)
     for match_num, match in enumerate(matches, start=1):
         bats_text = match.group('BATS')
-    df.at[index,'bats'] = bats_text
     print(index)
+
+    player = row.to_dict()
+    player['Bats'] = bats_text
+    players.append(player)
+
     # driver.get(base_url + ending)
     # elem = driver.find_element_by_xpath('//*[@id="meta"]/div[2]/p[2]')
     # print(elem.text)
 
 column_order.append('Bats')
-df[column_order].to_csv('output.csv', index=False)
+player_df = pd.DataFrame(players)
+player_df.to_csv('output.csv', columns=column_order, index=False)
 # driver.close()
